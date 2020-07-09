086cb92ef1be09ffc6b0bde027fd92694f4fa3bb
mainGithubSerega <67060763+mainGithubSerega@users.noreply.github.com>
2020-07-09 12:35:41 +0300

    added

--- js/script.js
+++ js/script.js
@@ -12,4 +12,77 @@ function addClass(selList, selBurger, selBody){
 }
 addClass('.header-top__nav', '.header-top__burger', 'body')
 
+const filter = () => {
+    const list = document.querySelector('.resent__list'),
+          items = list.querySelectorAll('a'),
+          btnAll = list.querySelector('.all'),
+          btnPrint = list.querySelector('.print'),
+          btnAnimation = list.querySelector('.animation'),
+          btnArt = list.querySelector('.art'),
+          btnWeb = list.querySelector('.web'),
+          btnPhotography = list.querySelector('.photography'),
+          btnVideo = list.querySelector('.video'),
+          wrapper = document.querySelector('.resent-box'),
+          filAll = wrapper.querySelectorAll('.all'),
+          filPrint = wrapper.querySelectorAll('.print'),
+          filAnimation = wrapper.querySelectorAll('.animation'),
+          filArt = wrapper.querySelectorAll('.art'),
+          filWeb = wrapper.querySelectorAll('.web'),
+          filPhotography = wrapper.querySelectorAll('.photography'),
+          filVideo = wrapper.querySelectorAll('.video');
+
+    const filters = (collection) => {
+        filAll.forEach(item => {
+            item.style.display = 'none'
+        })
+        if(collection){
+            collection.forEach(i => {
+                i.style.display = 'block'
+            })
+        } else {
+            return false
+        }
+    }
+    btnAll.addEventListener('click', () => {
+        filters(filAll)
+    })
+
+    btnPrint.addEventListener('click', () => {
+        filters(filPrint)
+    })
+
+    btnAnimation.addEventListener('click', () => {
+        filters(filAnimation)
+    })
+
+    btnArt.addEventListener('click', () => {
+        filters(filArt)
+    })
+
+    btnWeb.addEventListener('click', () => {
+        filters(filWeb)
+    })
+
+    btnPhotography.addEventListener('click', () => {
+        filters(filPhotography)
+    })
+
+    btnVideo.addEventListener('click', () => {
+        filters(filVideo)
+    })
+    list.addEventListener('click', (event) => {
+        event.preventDefault()
+        let targ = event.target
+        if(targ.tagName == 'A'){
+            items.forEach(index => {
+                index.classList.remove('active')
+                targ.classList.add('active')
+            })
+
+        }
+    })
+}
+filter()
+
+
 
